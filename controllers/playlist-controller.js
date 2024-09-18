0d5de1fa3a9b4463f641a1cfe84c5ded859986d7
Ashish-Gupta7 <ash300mr@gmail.com>
2024-09-18 22:24:45 +0530

    show playlist done

--- controllers/playlist-controller.js
+++ controllers/playlist-controller.js
@@ -14,7 +14,7 @@ const postCreatePlaylist = async (req, res) => {
     user: user._id,
   });
 
-  let saveUserPlaylist = await userModel.findOneAndUpdate(
+  await userModel.findOneAndUpdate(
     { _id: user._id },
     {
       $push: {
@@ -23,13 +23,31 @@ const postCreatePlaylist = async (req, res) => {
     },
     { new: true }
   );
-
-  // await saveUserPlaylist.save();
-  console.log(saveUserPlaylist);
-  console.log(saveUserPlaylist.playlists);
-
-  // let allPlaylists = await userModel.find({ _id: user._id });
-  // console.log(allPlaylists);
 };
 
-module.exports = { getCreatePlaylist, postCreatePlaylist };
+const showPlaylist = async (req, res) => {
+  let user = req.user;
+  let playlistIds = req.user.playlists;
+
+  // Use Promise.all to handle multiple asynchronous operations
+
+  // hum async-await ka use kar sakte hain, lekin jab aapko multiple asynchronous operations ek array ya list me handle karne hote hain, to har operation ko individually await karna inefficient hota hai. Is situation me Promise.all ko use karna zyada effective hota hai.
+
+  // Agar aap async-await ka use karte hain bina Promise.all ke, to har operation ek ke baad ek execute hoga, jo ki slow ho sakta hai. Promise.all parallel me sari promises ko execute karta hai, jo time-efficient hota hai.
+
+  let playlists = await Promise.all(
+    playlistIds.map(async (findId) => {
+      try {
+        let playlist = await playlistModel.findById(findId);
+        return playlist;
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    })
+  );
+
+  res.render("showPlaylist", { playlists });
+};
+
+module.exports = { getCreatePlaylist, postCreatePlaylist, showPlaylist };
--- routes/playlist.js
+++ routes/playlist.js
@@ -6,10 +6,13 @@ const { isLoggedIn } = require("../middlewares/login-middleware");
 const {
   getCreatePlaylist,
   postCreatePlaylist,
+  showPlaylist,
 } = require("../controllers/playlist-controller");
 
 router.get("/", isLoggedIn, getCreatePlaylist);
 
 router.post("/create", isLoggedIn, postCreatePlaylist);
+
+router.get("/show", isLoggedIn, showPlaylist);
 
 module.exports = router;
